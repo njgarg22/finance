829802b7b14e29c562c0e8374bfbe5cca91fee75
njgarg22 <njgarg22@gmail.com>
2020-09-02 22:42:48 +0530

    Add Entity, Repository and populate the db

--- src/main/java/com/neeraj/finance/FinanceApplication.java
+++ src/main/java/com/neeraj/finance/FinanceApplication.java
@@ -3,6 +3,8 @@ package com.neeraj.finance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication is a meta-annotation that pulls in component scanning, 
+//  autoconfiguration, and property support.
 @SpringBootApplication
 public class FinanceApplication {
 
