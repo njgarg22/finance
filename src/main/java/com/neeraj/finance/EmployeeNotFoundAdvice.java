ad06b0d99bbbfd98f68e06dbcc37ce092bc6c94d
Neeraj Garg <njgarg22@gmail.com>
2020-09-05 19:25:40 +0530

    Update EmployeeNotFoundAdvice.java

--- src/main/java/com/neeraj/finance/EmployeeNotFoundAdvice.java
+++ src/main/java/com/neeraj/finance/EmployeeNotFoundAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// When an `EmployeeNotFoundException` is thrown, this extra tidbit of Spring MVC configuration is used to render an `HTTP 404`:
 @ControllerAdvice
 class EmployeeNotFoundAdvice {
 
